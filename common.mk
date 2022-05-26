6ff67592abb7acdc7b9c2ea9000ce0565fd07ca7
Sam Wood <sam.wood@luthersystems.com>
2022-05-25 19:49:58 -0700

    Bump 0.0.52-SNAPSHOT

--- common.mk
+++ common.mk
@@ -2,7 +2,7 @@ PROJECT=buildenv
 IMAGE_PREFIX=luthersystems
 PROJECT_PATH=bitbucket.org/luthersystems/${PROJECT}
 
-VERSION=0.0.51
+VERSION=0.0.52-SNAPSHOT
 
 AWS_REGION=eu-west-2
 ECR_HOST=967058059066.dkr.ecr.${AWS_REGION}.amazonaws.com
